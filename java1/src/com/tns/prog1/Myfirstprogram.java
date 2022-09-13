8e31614efbd24f382a4dc044383d13b635159cbb
thejaswini <thejaswini.s24@gmail.com>
2022-09-13 11:48:01 +0530

    First commit

---
+++ java1/src/com/tns/prog1/Myfirstprogram.java
@@ -0,0 +1,10 @@
+package com.tns.prog1;
+
+public class Myfirstprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World!");
+	}
+
+}
